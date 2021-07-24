3a83d316a76e84598305790aedc30c1953eafbbf
Abhishek Jindal <abhishekjindal.jindal74@gmail.com>
2021-07-24 22:56:49 +0530

    Added input.cpp program

---
+++ input.cpp
@@ -0,0 +1,10 @@
+#include <iostream>
+
+int main (void)
+{
+    int num;
+    std::cout << "Enter a number: ";
+    std::cin >> num;
+    std::cout << "The number entered is: " << num << "\n";
+    return 0;
+}
